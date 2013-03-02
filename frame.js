ba2c77cf6c45f695f52132b098c9b5b69218eedc
wizCll <cll@wiz.cn>
2013-03-02 17:58:29 +0800

    刚开始了一个框架
    我要买iPhone~ 冠冕堂皇的理由是做HTML5移动开发~

---
+++ frame.js
@@ -0,0 +1,13 @@
+var userAgent = navigator.userAgent;
+userAgent ={
+}
+
+function isiDevice( userAgent ){
+	var ua = userAgent.toLowerCase();
+	if( ua.indexOf('iphone') || ua.indexOf('ipad') )
+	return os;
+}
+
+function checkBrowser(){
+	
+}
