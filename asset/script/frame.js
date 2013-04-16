ddbfe49135b129b06ba988c2aad9a967e9b5b73b
wizCll <cll@wiz.cn>
2013-04-16 18:41:25 +0800

    hammer的文件

--- asset/script/frame.js
+++ asset/script/frame.js
@@ -12,6 +12,44 @@ function checkBrowser(){
 	
 }
 
+$(document).ready(function(){
+	var $section = $('section');
+	var article = $('article').get(0);
+
+	var sCoordinate = {}, eCoordinate = {};
+
+	$section.hammer().on('dragstart', function( event ){
+		event.gesture.preventDefault()
+		sCoordinate.x = event.gesture.deltaX;
+		sCoordinate.y = event.gesture.deltaY;
+	});
+	
+	$section.hammer().on('dragend', function( event ){
+		eCoordinate.x = event.gesture.deltaX;
+		eCoordinate.y = event.gesture.deltaY;
+		if(eCoordinate.x - sCoordinate.x > 30){
+			$section.addClass('activeAside');
+			return false;
+		}
+
+		if( eCoordinate.x - sCoordinate.x < 0 ){
+			$section.removeClass('activeAside');
+			return false;
+		}
+	});
+
+	$section.hammer().on('tap', function(event){
+		 if( $section.hasClass('activeAside') ){
+		 	$section.removeClass('activeAside');
+		 	return false;
+		 }
+	})
+
+	$.get('http://127.0.0.1/')
+	
+});
+
 window.onload = function(){
-	console.log('script works.');
+
 }
+
